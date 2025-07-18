0c307fe1fbc3067b055d0a74d19eb269bf9a2f8e
jeongminji <parapin2274@gmail.com>
2025-07-18 15:19:19 +0900

    [Setting] #2 - Xcode 폴더링

---
+++ Ato-visionOS/Ato-visionOS/Source/Common/Protocol/Command.swift
@@ -0,0 +1,13 @@
+//
+//  Command.swift
+//  Ato-visionOS
+//
+//  Created by jeongminji on 7/18/25.
+//
+
+import Foundation
+
+/// 모든 커맨드가 채택할 프로토콜
+protocol Command {
+
+}
---
+++ Ato-visionOS/Ato-visionOS/Source/Enum/ToolType.swift
@@ -0,0 +1,18 @@
+//
+//  ToolType.swift
+//  Ato-visionOS
+//
+//  Created by jeongminji on 7/18/25.
+//
+
+import SwiftUI
+
+enum ToolType: CaseIterable, Equatable {
+    case grab
+    case magnify
+    case bond
+    case dissociate
+    case erase
+    case undo
+    case redo
+}
